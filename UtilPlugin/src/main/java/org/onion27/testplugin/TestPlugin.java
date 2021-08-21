8c7f1fd232106885c9bf670fbb5c9a4cf1982e4c
27Onion Nebell <57032603+onion108@users.noreply.github.com>
2021-08-21 10:38:58 +0800

    Update

--- UtilPlugin/src/main/java/org/onion27/testplugin/TestPlugin.java
+++ UtilPlugin/src/main/java/org/onion27/testplugin/TestPlugin.java
@@ -12,36 +12,49 @@ import org.bukkit.event.player.PlayerLoginEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
+@FunctionalInterface
+interface InputHandler {
+    void onInput(String message);
+}
 
 public final class TestPlugin extends JavaPlugin implements Listener {
-    private String[] OP_LIST = {"27Onion", "_AddMeInTopOfMc_", "SP_Sudierth"};
-    private String[] BUILDERS_LIST = {};
-    private String[] ARG0_COMMANDS = {
+    private final String[] OP_LIST = {"27Onion", "_AddMeInTopOfMc_", "SP_Sudierth"};
+    private final String[] BUILDERS_LIST = {};
+    private Map<String, InputHandler> inputBuffer = new HashMap<>();
+    private final String[] ARG0_COMMANDS = {
             "version", "home", "gamemode", "gmo", "echo", "init", "tp", "tpo", "fly", "down",
-            "setHealth", "me", "wolfys"
+            "setHealth", "me", "wolfys", "wallet", "say", "input"
     };
-    private  String[] HOME_COMMANDS = {
+    private final String[] HOME_COMMANDS = {
             "set", "visit", "go"
     };
-    private File homesSaveFile = new File(getDataFolder(),"./.player_home_info");
-    private YamlConfiguration homesConfig = new YamlConfiguration();
-    public final String pluginVersion = "SNAPSHOT-21d168b";
+    private  final String[] WALLET_COMMANDS = {
+            "check", "send"
+    };
+    private final File homesSaveFile = new File(getDataFolder(),"./.player_home_info");
+    private final File walletSaveFile = new File(getDataFolder(), "./.player_wallet_info");
+    private final YamlConfiguration homesConfig = new YamlConfiguration();
+    private final YamlConfiguration walletConfig = new YamlConfiguration();
+    public final String pluginVersion = "SNAPSHOT-21d169a";
 
     @Override
     public void onEnable() {
-        System.out.println("Enabled Plugin");
-        Bukkit.getPluginCommand("home").setExecutor(this);
+        System.out.println("Enabled Plugin Util!");
+        System.out.println("\n================================================");
+        getLogger().info("by 27Onion");
+        getLogger().info("Version: " + pluginVersion);
         Bukkit.getPluginCommand("util").setExecutor(this);
         getServer().getPluginManager().registerEvents(this, this);
         File configFile = homesSaveFile;
-        if(getDataFolder().exists()) {
+        if(!getDataFolder().exists()) {
             getDataFolder().mkdir();
+            getLogger().info("Make Directory Done");
+            getLogger().info("Wooh");
         }
+        getLogger().info("Data folder at: " + getDataFolder().getAbsolutePath());
         if(!configFile.exists()) {
             try {
                 configFile.createNewFile();
@@ -51,13 +64,33 @@ public final class TestPlugin extends JavaPlugin implements Listener {
         } else {
             try {
                 homesConfig.load(homesSaveFile);
+                getLogger().info("Loading save: " + homesSaveFile.getAbsolutePath());
+                getLogger().info("...");
             } catch (IOException e) {
                 getLogger().info("Fuck you");
             } catch (InvalidConfigurationException e) {
-                getLogger().info("And fuck you, too at line 52");
+                getLogger().info("And fuck you, too at line 72");
             }
             readConfig();
         }
+        if(!walletSaveFile.exists()) {
+            try {
+                walletSaveFile.createNewFile();
+            } catch(IOException e) {
+                getLogger().info("Can't create the wallet.");
+            }
+        } else {
+            try {
+                walletConfig.load(walletSaveFile);
+            } catch (IOException e) {
+                getLogger().info("Fuck Sudierth");
+            } catch (InvalidConfigurationException e) {
+                getLogger().info("Fuck 27Onion");
+            }
+        }
+        getLogger().info("Util: Config file at" + configFile.getAbsolutePath());
+        getLogger().info("Util: Config folder at" + getDataFolder().getAbsolutePath());
+        getLogger().info("\n=====================END======================");
     }
 
     @Override
@@ -69,6 +102,7 @@ public final class TestPlugin extends JavaPlugin implements Listener {
     private void writeConfig() {
         try {
             homesConfig.save(homesSaveFile);
+            walletConfig.save(walletSaveFile);
         } catch (IOException e) {
             getLogger().info("Never gonna give you up");
         }
@@ -261,6 +295,53 @@ public final class TestPlugin extends JavaPlugin implements Listener {
                                     break;
                             }
                             break;
+                        case "input":
+                            String msg = "";
+                            for(int i = 1; i < args.length; i++) {
+                                msg += args[i] + " ";
+                            }
+                            String name = player.getDisplayName();
+                            if(inputBuffer.containsKey(name)) {
+                                inputBuffer.get(name).onInput(msg);
+                            }
+                            break;
+                        case "wallet":
+                            switch (args[1]) {
+                                case "check":
+                                    if(walletConfig.contains(player.getDisplayName())) {
+                                        player.sendRawMessage(ChatColor.GREEN + player.getDisplayName() + "'s Wallet Info");
+                                        player.sendRawMessage(ChatColor.RESET + "You have " + ChatColor.RED + walletConfig.get(player.getDisplayName()) + ChatColor.RESET + " G.");
+                                    } else {
+                                        player.sendRawMessage("You don't have a wallet yet. We will help you to create one and put 20G in it.");
+                                        walletConfig.set(player.getDisplayName(), 20);
+                                        walletConfig.save(walletSaveFile);
+                                    }
+                                    break;
+                                case "send":
+                                    Player playerToSend = Bukkit.getPlayer(args[1]);
+                                    if(playerToSend != null && walletConfig.contains(player.getDisplayName()) && walletConfig.contains(playerToSend.getDisplayName())) {
+                                        Integer toSend = Integer.parseInt(args[2]);
+                                        Integer yourMoney = (Integer)walletConfig.get(player.getDisplayName());
+                                        Integer theirMoney = (Integer)walletConfig.get(playerToSend.getDisplayName());
+                                        if(yourMoney - toSend >= 0) {
+                                            yourMoney -= toSend;
+                                            theirMoney += toSend;
+                                            walletConfig.set(player.getDisplayName(), yourMoney);
+                                            walletConfig.set(playerToSend.getDisplayName(), theirMoney);
+                                            playerToSend.sendRawMessage(ChatColor.GREEN + "Hey guy! " + player.getDisplayName() + " gave you " + toSend + "G. What a kind person!");
+                                        } else {
+                                            player.sendRawMessage(ChatColor.RED + "You don't have enough money yet, poor man." + ChatColor.RESET);
+                                        }
+                                    } else {
+                                        player.sendRawMessage(ChatColor.RED + "An error occurred.");
+                                        player.sendRawMessage(ChatColor.RED + "Please check if you input a number, or the player to send don't have a wallet yet, or you don't have a wallet.");
+                                        player.sendRawMessage(ChatColor.RED + "Please run " + ChatColor.YELLOW + "/util " + ChatColor.BLUE + " wallet check" + ChatColor.RED + "  to check." + ChatColor.RESET);
+                                    }
+                            }
+                            break;
+                        default:
+                            player.sendRawMessage("FUCK! WHAT ARE YOU TYPING! WE CAN'T FIND " + args[0] + " !");
+                            break;
                     }
                 } catch(Exception e) {
                     return false;
@@ -284,9 +365,18 @@ public final class TestPlugin extends JavaPlugin implements Listener {
                         .collect(Collectors.toList());
             }
         }
+        if(args[0].equals("wallet")) {
+            if(args.length == 1) {
+                return Arrays.asList(WALLET_COMMANDS);
+            } else if(args.length == 2) {
+                return Arrays.stream(HOME_COMMANDS)
+                        .filter(s -> s.startsWith(args[1]))
+                        .collect(Collectors.toList());
+            }
+        }
         if(args.length == 1)  return Arrays.stream(ARG0_COMMANDS)
                 .filter(s -> s.startsWith(args[0]))
                 .collect(Collectors.toList());
-        return new ArrayList<>();
+        return super.onTabComplete(sender, command, alias, args);
     }
 }
